285d32bf3c0ce9551a4c8f25e1ec2e5f55e0420a
yd98 <43686253+yd98@users.noreply.github.com>
2019-02-18 19:58:56 -0500

    Version1 with only single login

--- app/src/main/java/yosman/dhar/stonybrook/edu/famfityosmanv1/MainActivity.java
+++ app/src/main/java/yosman/dhar/stonybrook/edu/famfityosmanv1/MainActivity.java
@@ -1,13 +1,56 @@
 package yosman.dhar.stonybrook.edu.famfityosmanv1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    //creating variables for the buttons/textviews/etc
+    private EditText Name;
+    private EditText Password;
+    private TextView Info;
+    private Button Login;
+    private int counter = 5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //assigns variables for specific button
+        Name= (EditText)findViewById(R.id.etname);
+        Password= (EditText)findViewById(R.id.etpassword);
+        Info = (TextView)findViewById(R.id.tvinfo);
+        Login= (Button)findViewById(R.id.btlogin);
+
+        //make buttons clickable
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validate(Name.getText().toString(),Password.getText().toString());
+            }
+        });
+    }
+    //function to ensure the login/logout is correct
+    private void validate(String username, String userpassword){
+        if ((username.equals("Yosman")) && (userpassword.equals("1234"))) {
+            Intent intent = new Intent(MainActivity.this, Main3Activity.class);
+            startActivity(intent);
+        } else {
+            counter--;
+
+            Info.setText("No of attempts remaining: " + String.valueOf(counter));
+
+            if(counter ==0){
+                Login.setEnabled(false);
+            }
+        }
     }
 }
